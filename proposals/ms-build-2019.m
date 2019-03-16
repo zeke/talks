68481f2bff58838bbcdeed8453859363b26fa0ef
Zeke Sikelianos <zeke@sikelianos.com>
2019-03-15 20:59:44 -0700

    add ms-build-2019 proposal

---
+++ proposals/ms-build-2019.m
@@ -0,0 +1,19 @@
+## Session Title
+
+Community-building and automation for open-source sustainability
+
+## Description
+
+Open-sourcing is about more than just making code public. It's about creating welcoming environments where people feel empowered to participate, and using automation to minimize human bottlenecks. The GitHub Flow is a creative process whereby code is continuously created, tested, reviewed, and delivered to customers. In this talk, Zeke will cover how teams at GitHub apply the principles of the GitHub Flow to open-source projects to make the them more fun, welcoming, and scalable.
+
+## Speaker
+
+Zeke Sikelianos (@zeke)
+
+## Speaker Tagline
+
+Internationalization Engineer at GitHub
+
+## Speaker Biography
+
+Zeke has been building developer tools for over a decade. He created the Swagger UI framework, the Node.js platform on Heroku, the npm website, and is now working to internationalize GitHub.
